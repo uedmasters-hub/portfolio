4e5848a395461f53c4a4333702dcd997266effbb
Ramesh <ramesh@ramsmandals-mac.local>
2026-06-08 19:18:41 +0530

    single line

--- 404.php
+++ 404.php
@@ -40,6 +40,7 @@ $badUrl = htmlspecialchars($_SERVER['REQUEST_URI'] ?? '');
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
 
+  <link rel="stylesheet" href="<?= BASE_PATH ?>/assets/css/preloader.css"/>
   <link rel="stylesheet" href="<?= BASE_PATH ?>/assets/css/variables.css"/>
   <link rel="stylesheet" href="<?= BASE_PATH ?>/assets/css/reset.css"/>
   <link rel="stylesheet" href="<?= BASE_PATH ?>/assets/css/main.css"/>
